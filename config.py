cf8da8f0259d7917cdb1041fc082969e3eb7a99c
MathiasGermandsen <mgermandsen@gmail.com>
2026-05-04 11:20:43 +0200

    ye

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+USER_ID = os.environ["USER_ID"]
+ADMIN_KEY = os.environ["ADMIN_KEY"]
+HOSTNAME = os.environ["HOSTNAME"]
+AUDIENCE = os.environ["AUDIENCE"]
+OLD_JTI = os.environ["OLD_JTI"]
+SCOPES = os.environ["SCOPES"].split(",")
+SERVICE = os.environ["SERVICE"]
